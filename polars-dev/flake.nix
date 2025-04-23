427dceef3e58e2a9c3bc440ebd46c8d615789170
Lucio Franco <luciofranco14@gmail.com>
2025-04-23 11:34:59 -0400

    fix ld path polars

--- polars-dev/flake.nix
+++ polars-dev/flake.nix
@@ -144,35 +144,36 @@
           shellHook = ''
             export VENV=$(git rev-parse --show-toplevel)/.venv
 
-            # Set `nix-ld` env vars for nixos users that need these to be able
-            # to run `ruff`.
-            export NIX_LD=${pkgs.stdenv.cc.bintools.dynamicLinker}
-            export NIX_LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath linuxOnlyPkgs}
-
-            # Jemmalloc compiled with gcc doesn't like when we ask for the
-            # compiler to compile with fortify source so lets enable everything
-            # but fortify and fortify3.
-            export NIX_HARDENING_ENABLE="bindnow format pic relro stackclashprotection stackprotector strictoverflow zerocallusedregs"
-
-            # Set openssl for `cargo test` to work.
-            export LD_LIBRARY_PATH=${pkgs.openssl_3_4.out}/lib:$LD_LIBRARY_PATH
-
-            export PYO3_NO_REOCOMPILE=1
-            export PYO3_NO_RECOMPILE=1
-
-            export PYO3_PYTHON=$($VENV/bin/python -c "import sys,os; print(os.path.abspath(sys.executable))")
-            export PYTHON_SHARED_LIB=$($VENV/.venv/bin/python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
-
-            export POLARS_CLOUD_REST_DOMAIN_PREFIX=main.rest.api
-            export POLARS_CLOUD_GRPC_DOMAIN_PREFIX=main.grpc.api
-            export POLARS_CLOUD_DOMAIN=dev.cloud.pola.rs
-
             if [ ! -d $VENV ]; then
               uv venv $VENV
 
               unset CONDA_PREFIX \
                  &&  MATURIN_PEP517_ARGS="--profile dev" uv pip install -r requirements-dev.txt  
             fi
+
+            # Jemmalloc compiled with gcc doesn't like when we ask for the
+            # compiler to compile with fortify source so lets enable everything
+            # but fortify and fortify3.
+            export NIX_HARDENING_ENABLE="bindnow format pic relro stackclashprotection stackprotector strictoverflow zerocallusedregs"
+
+            
+            export PYO3_NO_REOCOMPILE=1
+            export PYO3_NO_RECOMPILE=1
+
+            export PYO3_PYTHON=$($VENV/bin/python -c "import sys,os; print(os.path.abspath(sys.executable))")
+            export PYTHON_SHARED_LIB=$($VENV/bin/python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+
+            # Set `nix-ld` env vars for nixos users that need these to be able
+            # to run `ruff`.
+            export NIX_LD=${pkgs.stdenv.cc.bintools.dynamicLinker}
+            export NIX_LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath linuxOnlyPkgs}:$PYTHON_SHARED_LIB"
+            # Set openssl for `cargo test` to work.
+            export LD_LIBRARY_PATH=${pkgs.openssl_3_4.out}/lib:$PYTHON_SHARED_LIB:$LD_LIBRARY_PATH
+
+
+            export POLARS_CLOUD_REST_DOMAIN_PREFIX=main.rest.api
+            export POLARS_CLOUD_GRPC_DOMAIN_PREFIX=main.grpc.api
+            export POLARS_CLOUD_DOMAIN=dev.cloud.pola.rs
 
             ${config.pre-commit.installationScript}
 
